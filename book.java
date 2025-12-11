101a9f511f7400efc0620cea75a6c96681a66c4a
Harsha-csb <harshavardhan619561@gmail.com>
2025-12-11 09:54:51 +0530

    Add Book class for library management system

--- book.java
+++ book.java
@@ -1,1 +1,19 @@
+package LibraryManagementSystem;
 
+public class Book {
+	int id;
+	String name;
+	String status;
+	 public Book(int id,String name,String status) {
+		 this.id=id;
+		 this.name=name;
+		 this.status=status;
+	 }
+	 public String toString() {
+		return "Book ID: " + id + ", Name: " + name + ", Status: " + status;
+		 
+	 }
+	 
+
+
+}
